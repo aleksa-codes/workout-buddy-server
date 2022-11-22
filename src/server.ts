3f8a6805001796360d240759645af772add63b41
aleksa-codes <stojsic.aleksa@gmail.com>
2022-11-22 18:21:04 +0100

    Fix: path to .env in production

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '/etc/secrets/.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
